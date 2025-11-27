aeddb1dd596dc66a7d374a0ebd0d8086fa3b0e32
Danitinhg <dani111hg@gmail.com>
2025-11-27 23:39:50 +0100

    Cambios en level01 y 03 | Arreglo el menu opciones relacionado a la musica

--- scripts/autoloads/game_manager.gd
+++ scripts/autoloads/game_manager.gd
@@ -10,9 +10,40 @@ var level_checkpoints: Dictionary = {}
 var checkpoint_collected_items: Dictionary = {}
 var temp_collected_items: Array[int] = []
 
+const SETTINGS_PATH = "user://settings.cfg"
+
 func _ready():
+	load_and_apply_settings()
 	load_levels()
 	PlayerData.load_game()
+
+func load_and_apply_settings():
+	var config = ConfigFile.new()
+
+	if FileAccess.file_exists(SETTINGS_PATH):
+		config.load(SETTINGS_PATH)
+
+	#audio
+	var master_volume = config.get_value("audio", "master_volume", 0.8)
+	var music_volume = config.get_value("audio", "music_volume", 0.7)
+	var sfx_volume = config.get_value("audio", "sfx_volume", 0.8)
+
+	AudioServer.set_bus_volume_db(0, linear_to_db(master_volume))
+
+	var music_bus = AudioServer.get_bus_index("Music")
+	if music_bus != -1:
+		AudioServer.set_bus_volume_db(music_bus, linear_to_db(music_volume))
+
+	var sfx_bus = AudioServer.get_bus_index("SFX")
+	if sfx_bus != -1:
+		AudioServer.set_bus_volume_db(sfx_bus, linear_to_db(sfx_volume))
+
+	#video
+	var fullscreen = config.get_value("video", "fullscreen", false)
+	DisplayServer.window_set_mode(
+		DisplayServer.WINDOW_MODE_FULLSCREEN if fullscreen
+		else DisplayServer.WINDOW_MODE_WINDOWED
+	)
 	
 func load_levels():
 	levels = [
