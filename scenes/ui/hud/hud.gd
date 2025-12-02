cdbf0229b5388db1a329c82e11633eb833bd4ff1
Danitinhg <dani111hg@gmail.com>
2025-12-02 11:36:03 +0100

    Superposición del menu de pause y options sobre el dialogo de Tin | Añadida barra de vida para el player

--- scenes/ui/hud/hud.gd
+++ scenes/ui/hud/hud.gd
@@ -4,6 +4,7 @@ extends CanvasLayer
 @onready var diamond2 = $VBoxContainer/DiamondContainer/Diamond2
 @onready var diamond3 = $VBoxContainer/DiamondContainer/Diamond3
 @onready var coin_label = $VBoxContainer/CoinContainer/CoinLabel
+@onready var health_fill: TextureProgressBar = $HealthFill
 
 @export var diamond_empty_texture: Texture2D
 @export var diamond_filled_texture: Texture2D
@@ -20,6 +21,12 @@ func _ready():
 
 	# Conectar a todos los coleccionables
 	connect_to_collectibles()
+
+	await get_tree().process_frame
+	var player = get_tree().get_first_node_in_group("player")
+	if player:
+		player.health_changed.connect(_on_player_health_changed)
+		_on_player_health_changed(player.current_health, player.max_health)
 
 func connect_to_collectibles():
 	var collectibles = get_tree().get_nodes_in_group("collectibles")
@@ -51,4 +58,9 @@ func update_diamond_display():
 		diamond3.texture = diamond_filled_texture if 2 in GameManager.temp_collected_items else diamond_empty_texture
 
 func update_coin_display():
-	coin_label.text = "x %02d" % current_coins
+	coin_label.text = "%02d x" % current_coins
+
+func _on_player_health_changed(current_health: int, max_health: int):
+	if health_fill:
+		health_fill.max_value = max_health
+		health_fill.value = current_health
