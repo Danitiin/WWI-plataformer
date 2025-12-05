ae73d239792b8cae245029e3e2d7cceb47161123
Danitinhg <dani111hg@gmail.com>
2025-12-05 10:04:40 +0100

    Eliminación de assets

--- level_test.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-@onready var player = $Player
-
-func _ready():
-	#Se añaden las habiliddades del player manualmente
-	var all_abilities = ["Dash", "DoubleJump", "Glide"]
-	for ability_name in all_abilities:
-		player.add_ability(ability_name)
